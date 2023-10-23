c91bc5cae0fd75b968bcfaa70fe671930564d73f
anasrazaofficial <anasbukhari4114@gmail.com>
2023-10-23 20:59:06 +0500

    Compile currency converter

--- currency-converter/src/hooks/useCurrencyInfo.js
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -1,6 +1,12 @@
-
+import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
-    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-    fetch(url).then((res) => console.log(res))
+    const [data, setdata] = new useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setdata(res[currency]))
+    }, [currency])
+    return data
 }
+
 export default useCurrencyInfo
