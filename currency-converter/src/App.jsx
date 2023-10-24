0e49f07cf724dccf64dfa8981a568644d43bd349
anasrazaofficial <anasbukhari4114@gmail.com>
2023-10-24 15:26:36 +0500

    Remove extra code

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -51,52 +51,5 @@ function App() {
       </div>
     </div>
   )
-
-  return (
-    <div
-      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-
-    >
-      <div className="w-full">
-        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              convert()
-
-            }}
-          >
-            <div className="w-full mb-1">
-              <InputBox
-                label="From"
-
-
-              />
-            </div>
-            <div className="relative w-full h-0.5">
-              <button
-                type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-
-              >
-                swap
-              </button>
-            </div>
-            <div className="w-full mt-1 mb-4">
-              <InputBox
-                label="To"
-                amount={convertedAmount}
-                currencyOptions={options}
-
-              />
-            </div>
-            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
-
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
 }
 export default App
