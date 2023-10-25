60e2c66726277e00d5b626952fe096eee2e98b18
anasrazaofficial <anasbukhari4114@gmail.com>
2023-10-25 17:51:03 +0500

    Compile Simple form

---
+++ mini-context/src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React, { useContext, useState } from 'react'
+import UserContext from '../context/UserContext'
+
+function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const { setUser } = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUser({ username, password })
+    }
+    return (
+        <div>
+            <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login
