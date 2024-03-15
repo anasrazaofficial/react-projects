e95abfcc3b9ae95d3a7528a67b259d048c5fc3d8
anasrazaofficial <anasbukhari4114@gmail.com>
2024-03-15 17:16:27 +0500

    Remove main code

--- src/Pages/index.js
+++
@@ -1,8 +0,0 @@
-export { Index } from "./Index.jsx";
-export { Counter } from "./Counter";
-export { CurrencyConverter } from "./Currency-Converter";
-export { BackgroundChanger } from "./Background-Changer";
-export { ModeToggler } from "./Mode-Toggler";
-export { PasswordGenerator } from "./Password-Generator";
-export { Todo } from "./Todo";
-export { Weather } from "./Weather";
