df94ddeda10786444b1a3e33f47768375d721fe9
anasrazaofficial <anasbukhari4114@gmail.com>
2024-03-15 17:15:29 +0500

    Make ui and half functionality

---
+++ rock-paper-scissors/src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from 'react'
+import { FaRegHandRock, FaRegHandScissors, FaRegHandPaper } from "react-icons/fa";
+
+const App = () => {
+  const [yourCount, setYourCount] = useState(0)
+  const [computerCount, setComputerCount] = useState(0)
+  const compMove = useRef()
+  const result = useRef()
+
+
+  const play = (e) => {
+    let random = Math.floor(Math.random() * 3)
+    let move = compMove.current.children[random].id
+    if (move === e) {
+      console.log(`Computer move: ${move} & Your move: ${e}`);
+      console.log('Match tied');
+    } else if ((move === 'paper' && e === 'scissor') ||
+      (move === 'rock' && e === 'paper') ||
+      (move === 'scissor' && e === 'rock')) {
+      console.log("You win");
+      setYourCount(yourCount + 1)
+    } else if ((move === 'rock' && e === 'scissor') ||
+      (move === 'scissor' && e === 'paper') ||
+      (move === 'paper' && e === 'rock')) {
+      console.log("Computer wins");
+      setComputerCount(computerCount + 1)
+    }
+    console.log(`Your move: ${e} & Computer move: ${move}`);
+  }
+  return (
+    <main className='h-screen w-screen bg-yellow-100 space-y-3'>
+      <h1 className='text-center' > Rock Paper Scissor</h1>
+
+
+      <section className='flex w-fit mx-auto gap-x-20'>
+        <div className='flex flex-col items-center gap-y-3'>
+          <h2 className='text-right'>You</h2>
+          <h3 className='text-right'>{yourCount}</h3>
+        </div>
+        <div className='flex flex-col items-center gap-y-3'>
+          <h2>Computer</h2>
+          <h3>{computerCount}</h3>
+        </div>
+      </section>
+
+
+      <section className='space-y-3 select-none'>
+        <h3 className='text-center'>Choose your move</h3>
+        <div className='flex justify-center'>
+          <div className='bg-yellow-100 hover:bg-yellow-200 rounded-md p-4 cursor-pointer' onClick={() => play('rock')} title='Rock'>
+            <FaRegHandRock className='text-6xl sm:text-8xl lg:text-9xl' />
+          </div>
+          <div className='bg-yellow-100 hover:bg-yellow-200 rounded-md p-4 cursor-pointer' onClick={() => play('paper')} title='Paper'>
+            <FaRegHandPaper className='text-6xl sm:text-8xl lg:text-9xl' />
+          </div>
+          <div className='bg-yellow-100 hover:bg-yellow-200 rounded-md p-4 cursor-pointer' onClick={() => play('scissor')} title='Scissor'>
+            <FaRegHandScissors className='text-6xl sm:text-8xl lg:text-9xl' />
+          </div>
+        </div>
+      </section>
+
+
+
+      <section className='space-y-3 select-none'>
+        <h3 className='text-center'>Computer move</h3>
+        <div className='flex justify-center' ref={compMove}>
+          <div id='rock' className='bg-yellow-100 rounded-md p-4'> <FaRegHandRock className='text-6xl sm:text-8xl lg:text-9xl' /> </div>
+          <div id='paper' className='bg-yellow-100 rounded-md p-4'> <FaRegHandPaper className='text-6xl sm:text-8xl lg:text-9xl' /> </div>
+          <div id='scissor' className='bg-yellow-100 rounded-md p-4'> <FaRegHandScissors className='text-6xl sm:text-8xl lg:text-9xl' /> </div>
+        </div>
+      </section>
+
+
+      <h3 ref={result}></h3>
+
+    </main>
+  )
+}
+
+export default App
